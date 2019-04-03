91c6544d056504e5102e4935d9b0b8f0b7011da2
Joonho Ko <joonhok@mit.edu>
2019-04-03 19:16:04 -0400

    added process.env.PORT for heroku

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
 app.get('/projects', (req, res) => res.sendFile('projects.html', { root: __dirname }))
 app.get('/classes', (req, res) => res.sendFile('classes.html', { root: __dirname }))
 
-app.listen(3000, () => console.log('running'))
+app.listen(process.env.PORT || 3000, () => console.log('running'))
