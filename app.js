07804b63348c8ced1b195e93a161eec83ab8a9d5
Joonho Ko <joonhok@mit.edu>
2019-09-29 17:33:54 -0400

    added colors-of-fifths

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
 app.get('/projects', (req, res) => res.sendFile('projects.html', { root: __dirname }))
 app.get('/classes', (req, res) => res.sendFile('classes.html', { root: __dirname }))
+app.get('/color', (req, res) => res.sendFile('color.html', { root: __dirname }))
 
 app.listen(process.env.PORT || 3000, () => console.log('running'))
